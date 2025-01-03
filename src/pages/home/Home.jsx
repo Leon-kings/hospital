882e4226271ca023072642d5bbab724d80515d11
Leon-kings <leonakingeneye2002@gmail.com>
2025-01-02 23:51:48 -0800

    changes on new components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,11 @@ import Blog from "../../compounds/blog/BlogIcons";
 import Count from "../../compounds/count/Count";
 // import Card from '../../compounds/card/CardTimeTable';
 import Slider from "../../compounds/slidebar/SliderHead";
+import Contact from '../contact/Address';
+import About1 from '../about/About';
+import Head from '../../compounds/heading/Head';
+import Carousel from '../../compounds/multislider/MultiSlider';
+import Service from '../services/Services';
 export default function Home() {
   return (
     <>
@@ -17,13 +22,21 @@ export default function Home() {
         <div className="contain">
           <Count />
         </div>
-        <div className="contain flex">
-           
+        <div className="contain ">
+           <Contact/>
         </div>
-        <div className="contain flex"></div>
-        <div className="contain flex"></div>
-        <div className="contain flex"></div>
-        <div className="contain flex"></div>
+        <div className="contain">
+          <About1/>
+        </div>
+        <div className="contain ">
+          <Head/>
+        </div>
+        <div className="contain flex">
+          <Carousel/>
+        </div>
+        <div className="contain flex">
+          <Service/>
+        </div>
        
       </div>
     </>
